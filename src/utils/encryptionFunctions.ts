825eb2bc8c0c4ead6b92cf4ca5335c20fedd9afd
Madame-Aehm <emilyrachelstickler@gmail.com>
2024-07-01 10:40:18 +0200

    prefer dark mode table style

--- src/utils/encryptionFunctions.ts
+++ src/utils/encryptionFunctions.ts
@@ -1,34 +1,15 @@
-export function encrypt1(plaintextValue: string, keyValue: number) {
-  const plaintext = plaintextValue.toLowerCase();
-  let ciphertext = "";
-  const { alphabet, alphabetShifted } = alphabets(keyValue);
-  for (let i = 0; i < plaintext.length; i++) {
-    if (plaintext[i] !== " ") {
-      for (let j = 0; j < alphabetShifted.length; j++) {
-        if (alphabet[j] === plaintext[i]) {
-          ciphertext += alphabetShifted[j];
-          break;
-        }
-      }
-    } else {
-      ciphertext += " ";
-    }
-  }
-  return ciphertext;
-}
-
 export function encrypt(plaintextValue: string, keyValue: number) {
   const { alphabet, alphabetShifted } = alphabets(keyValue);
   return plaintextValue.split("").map((char) => {
     if (alphabet.includes(char.toLowerCase())) {
-      const alphIndex = getAlphIndex(char);
+      const alphIndex = getCharIndex(char);
       return char.toUpperCase() === char ? alphabetShifted[alphIndex].toUpperCase() : alphabetShifted[alphIndex]; 
     }
     return char;
   }).join("")
 }
 
-function getAlphIndex (char: string) {
+function getCharIndex (char: string) {
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
   for (let i = 0; i < alphabet.length; i++) {
     if (char.toLowerCase() === alphabet[i]) {
